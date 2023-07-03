15af60848495db4e988006fe7cc56134a846e967
naina-yoganathan <136118419+naina-yoganathan@users.noreply.github.com>
2023-07-03 11:32:27 +0530

    using key in sort()

--- captains_wins.py
+++ captains_wins.py
@@ -1,8 +1,12 @@
 """
-    sort by number of wins 
+    sort by number of wins using key in sort()
 """
 
 import csv
+
+def get_wins(tup):
+    return tup[1]
+
 with open("captains.txt") as FH:
     data = csv.reader(FH)
     header = next(data)
@@ -11,17 +15,17 @@ with open("captains.txt") as FH:
         # row[0] is captain name
         # row[3] is no. of wins
         # print(f"{row[0]:14} {row[3]:>3}") 
-        info = (int(row[3]), row[0])
+        info = (row[0], int(row[3]))
         captains_wins.append(info)
 
-    captains_wins.sort()
+    captains_wins.sort(key = get_wins)
     for captain in captains_wins:
-        print(f"{captain[1]:14}{captain[0]:>3}")
+        print(f"{captain[0]:14}{captain[1]:>3}")
 
-    print("first captain is: ", captains_wins[0][1])
-    print("last captain is: ", captains_wins[-1][1])
-
+    print("first captain is: ", captains_wins[0][0])
+    print("last captain is: ", captains_wins[-1][0])
+    
     for captain in captains_wins:
-        if captain[1] == "V Kohli":
-            print("No. of wins for V kohli: ", captain[0])
+        if captain[0] == "V Kohli":
+            print("No. of wins for V kohli: ", captain[1])
             break
