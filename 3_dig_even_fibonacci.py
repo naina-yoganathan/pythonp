d1a819f2e677a5fb0658a512441a549dc62ec1b3
naina-yoganathan <136118419+naina-yoganathan@users.noreply.github.com>
2023-07-13 16:53:48 +0530

    Add files via upload

---
+++ 3_dig_even_fibonacci.py
@@ -0,0 +1,14 @@
+n1=0
+n2=1
+n = n1 + n2
+while n < 1000:
+    n=n1+n2
+    # if n in range(100,1000):
+    if n>100 and n<1000:
+        if ((n%2) == 0):
+            print(n)
+    n1 = n2
+    n2 = n
+
+#144 			
+#610 	
